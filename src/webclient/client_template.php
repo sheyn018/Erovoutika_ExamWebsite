981d8e83f0b9df37824615dd3aaba295ea28aa9b
CorvusOps <josepholivar124@gmail.com>
2022-09-29 10:41:04 +0800

    Hotfix and Clean pathfiles, file names, sync of multiple files

--- src/webclient/client_template.php
+++ src/webclient/client_template.php
@@ -36,7 +36,7 @@ include '../includes/connectdb.php';
 			if($_SESSION['staff_sid']==session_id()){
 				header("location:404.php");		
 			}else{
-				header("location:login.php");
+				header("location:login_template.php");
 			}
 		}
 	}
